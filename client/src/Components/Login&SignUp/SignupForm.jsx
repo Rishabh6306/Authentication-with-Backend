782f73c6a34800983a9872a945a0310bf94c4de0
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-08-13 01:08:17 +0530

    Login and Registration form Backend Part Started

--- client/src/Components/Login&SignUp/SignupForm.jsx
+++ client/src/Components/Login&SignUp/SignupForm.jsx
@@ -1,29 +1,37 @@
 import './Login&SignupForm.css'
 import React, { useState } from 'react'
-import axois from 'axios'
+import axios from 'axios'
 
 function SignupForm({ toggleForm }) {
-const [name, setName] = useState()
-const [email, setEmail] = useState()
-const [password, setPassword] = useState()
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [message, setMessage] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
 
-const handleSubmit = (e) => {
-  e.preventDefault(); // Prevent the default form submission behavior
-
-  axois.post('http://localhost:3001/register', {name, email, password})
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error))
-}
+    try {
+      const response = await axios.post('http://localhost:3001/register', {
+        email,
+        password,
+      });
+      setMessage(response.data.message);
+    } catch (error) {
+      setMessage('An error occurred');
+      console.error(error);
+    }
+  };
 
   return (
     <div className="container">
       <form className="signUp-form" onSubmit={handleSubmit}>
         <h1>Sign Up</h1>
-        <input type="text" placeholder='Name' onChange={(e) => setName(e.target.value)}/>
-        <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
-        <input type="password" placeholder='Paasword' onChange={(e) => setPassword(e.target.value)}/>
+        <input type="text" placeholder='Name' autoComplete="name" onChange={(e) => setName(e.target.value)} />
+        <input type="email" placeholder='Email' autoComplete="email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" placeholder='Password' autoComplete="current-password" onChange={(e) => setPassword(e.target.value)} />
         <input type="submit" className='button' />
+
         <pre>Have an Account! <button onClick={toggleForm}>Login</button></pre>
       </form>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,37 +1,38 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import { config as configDotenv } from 'dotenv';
-import emloyeeModel from './models/employee';
 
 configDotenv();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors()); 
+
+app.use(cors({ origin: 'http://localhost:5173' })); 
 
 const connectToDb = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
         });
-        console.log(`Connected to MongoDB`);
-    } catch (err) {
-        console.error('Error connecting to MongoDB:', err.message);
-        process.exit(1); // Exit the process with a non-zero status code
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error.message);
+        process.exit(1);
     }
 };
 
 connectToDb();
 
-app.post('/', (req, res) => {
-     emloyeeModel.create(req.body)
-     .then((employees) => res.json())
-     .catch((err) => res.json(err))
-})
+app.post('/register', (req, res) => {
+    // Handle user registration logic here
+    // For now, let's just send a response
+    res.json({ message: 'User registered successfully' });
+});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
