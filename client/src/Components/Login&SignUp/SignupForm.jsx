9666243e02498abdc33dc186fa6fdbe3149a9d6b
Rishabh Srivastava <rishabhsrivastav722@gmail.com>
2023-08-13 17:21:30 +0530

    Optimized code structure.

--- client/src/Components/Login&SignUp/SignupForm.jsx
+++ client/src/Components/Login&SignUp/SignupForm.jsx
@@ -3,30 +3,37 @@ import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
 
 function SignupForm() {
+  // State variables to store form field values and messages
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [message, setMessage] = useState('');
+
+  // React Router's hook for navigation
   const navigate = useNavigate();
 
+  // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
+      // Send user registration data to the server
       const response = await axios.post('http://localhost:3001/register', {
         name,
         email,
         password,
       });
 
+      // Display success message, log server response, and navigate to login
       setMessage('User registered successfully');
       console.log('Form submitted successfully:', response.data);
       navigate('/login'); // Redirect to the login page
     } catch (error) {
+      // Handle errors: Display custom error messages or log generic error
       if (error.response && error.response.data && error.response.data.error) {
         setMessage('User already exists'); // Set custom error message
       } else {
-        setMessage('An error occurred');
+        setMessage('An error occurred'); // Set generic error message
         console.error(error);
       }
     }
@@ -36,6 +43,7 @@ function SignupForm() {
     <div className="container">
       <form className="signUp-form" onSubmit={handleSubmit}>
         <h1>Sign Up</h1>
+        {/* Input fields for name, email, and password */}
         <input
           type="text"
           placeholder="Name"
@@ -57,10 +65,13 @@ function SignupForm() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+        {/* Submit button */}
         <input type="submit" className="button" />
+        {/* Link to the login page */}
         <pre>
-          Have an Account! <Link className='btn' to="/login">Login</Link>
+          Already have an account? <Link className='btn' to="/login">Login</Link>
         </pre>
+        {/* Display success or error message */}
         <p>{message}</p>
       </form>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,69 +1,27 @@
 import express from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
 import { config as configDotenv } from 'dotenv';
-import employeeModel from './models/employee.js';
+import { connectToDb } from './config/db.js';
+import router from './routes/routes.js'
 
+// Load environment variables from .env file
 configDotenv();
 
+// Create an Express app
 const app = express();
 app.use(express.json());
 
-// Adjust the CORS configuration to allow connections from localhost:5173
+// Configure CORS to allow connections from http://localhost:5173
 app.use(cors({ origin: 'http://localhost:5173' }));
 
-const connectToDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('Connected to MongoDB');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error.message);
-        process.exit(1);
-    }
-}
-
+// Connect to MongoDB
 connectToDb();
 
-app.post('/register', async (req, res) => {
-    const { name, email, password } = req.body;
+// Attach the router to handle routes
+app.use('/', router);
 
-    try {
-        const existingEmployee = await employeeModel.findOne({ email: email });
-        if (existingEmployee) {
-            return res.status(400).json({ error: 'Email is already registered' });
-        }
-
-        const newEmployee = await employeeModel.create({ name, email, password });
-        res.json(newEmployee);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
-
-app.post('/login', (req, res) => {
-    const { email, password } = req.body;
-    employeeModel.findOne({ email: email })
-        .then(user => {
-            if (!user) {
-                return res.json('No record existed');
-            }
-
-            if (user.password.trim() === password) {
-                res.json('Success!');
-            } else {
-                res.json('Incorrect password');
-            }
-        })
-        .catch(error => {
-            res.status(500).json({ error: error.message });
-        });
-});
-
+// Start the server
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
